d59f9e6baf63ad8065642affa08cfef938e8eb49
fincci <lp-oliveirac@hotmail.com>
2022-06-13 02:45:53 -0300

    finish selectPokemon and add firstPokemon

--- src/js/index.js
+++ src/js/index.js
@@ -6,7 +6,106 @@ const pokemonsCards = document.querySelectorAll('.card-pokemon')
 
 
 mouseSounds();
+firstPokemon();
 selectPokemon();
+
+function firstPokemon() {
+    let url = `https://pokeapi.co/api/v2/pokemon/pikachu`
+            fetch(url)
+                .then((response) => {
+                    return response.json();
+                })
+                .then((data) => {
+                    // console.log(data);
+                    changeName();
+                    changeID();
+                    changeType();
+                    changeImg();
+                    changeBg();
+                    changeStatus();
+                    changeSkills();
+
+                    function changeName() {
+                        const name = document.getElementById('name')
+                        name.innerText = `${data.name[0].toUpperCase()}${data.name.substring(1)}`
+                    }
+
+                    function changeID() {
+                        const pokePlaceID = document.getElementById('pokeID')
+                        let idText = addZeros(data.id, 3)
+                        pokePlaceID.innerText = `#${idText}`
+                        function addZeros(num, length) {
+                            return String(num).padStart(length, '0')
+                        }
+                    }
+
+                    function changeType() {
+                        const typePlace = document.getElementById('type')
+                        const rawTypes = data.types
+                        let types = []
+                        let spanLength = document.querySelectorAll('.type').length
+                        if (spanLength !== types.length) {
+                            typePlace.innerHTML = ''
+                        }
+                        for (let i = 0; i < rawTypes.length; i++) {
+                            let typesValue = rawTypes[i].type.name;
+                            types.push(typesValue)
+                            typePlace.innerHTML += `<span class="type">${typesValue[0].toUpperCase()}${typesValue.substring(1)}</span>`
+                        }
+                    }
+
+                    function changeImg() {
+                        const imgPlace = document.getElementById('card-img')
+                        let imgPokemon = data.sprites.other.home.front_default
+                        imgPlace.src = imgPokemon
+                    }
+
+                    function changeBg() {
+                        const cardBg = document.getElementById('card-pokemon')
+                        const rawTypesBg = data.types
+                        let typesBg = []
+                        for (let i = 0; i < rawTypesBg.length; i++) {
+                            let typesValueBg = rawTypesBg[i].type.name;
+                            typesBg.push(typesValueBg)
+                        }
+                        if (typesBg.length === 1) {
+                            let colorBg = `var(--${typesBg})`
+                            cardBg.style.background = colorBg
+                        } else {
+                            let colorBg = `linear-gradient(90deg, var(--${typesBg[0]}), var(--${typesBg[1]}))`
+                            cardBg.style.backgroundImage = colorBg
+                        }
+                    }
+
+                    function changeStatus() {
+                        const statsApi = data.stats
+                        const statsUl = document.querySelector('.stats')
+                        if (statsApi !== statsUl) {
+                            statsUl.innerHTML = ''
+                        }
+                        for (let i = 0; i < statsApi.length; i++) {
+                            let statsName = statsApi[i].stat.name;
+                            let statsValue = statsApi[i].base_stat
+                            statsUl.innerHTML += `<li><p>${statsName[0].toUpperCase()}${statsName.substring(1).replace('-', ' ')}:</p> <i>${statsValue}</i>`
+                        }
+                    }
+
+                    function changeSkills() {
+                        const skillsApi = data.abilities
+                        const skillsUl = document.querySelector('.skills')
+                        if (skillsApi.length !== skillsUl.length) {
+                            skillsUl.innerHTML = ''
+                        }
+                        for (let i = 0; i < skillsApi.length; i++) {
+                            let skill = skillsApi[i].ability.name;
+                            skillsUl.innerHTML += `<li class="skill">${skill[0].toUpperCase()}${skill.substring(1).replace('-', ' ')}</li>`
+                        }
+                    }
+                })
+                .catch((err) => {
+                    console.log(`Erro: ${err}`);
+                });
+}
 
 function selectPokemon() {
     pokemonList.forEach(pokemon => {
@@ -36,7 +135,6 @@ function selectPokemon() {
                         const pokePlaceID = document.getElementById('pokeID')
                         let idText = addZeros(data.id, 3)
                         pokePlaceID.innerText = `#${idText}`
-
                         function addZeros(num, length) {
                             return String(num).padStart(length, '0')
                         }
@@ -65,31 +163,32 @@ function selectPokemon() {
 
                     function changeBg() {
                         const cardBg = document.getElementById('card-pokemon')
-                        const rawTypes = data.types
-                        let types = []
-                        for (let i = 0; i < rawTypes.length; i++) {
-                            let typesValue = rawTypes[i].type.name;
-                            types.push(typesValue)
+                        const rawTypesBg = data.types
+                        let typesBg = []
+                        for (let i = 0; i < rawTypesBg.length; i++) {
+                            let typesValueBg = rawTypesBg[i].type.name;
+                            typesBg.push(typesValueBg)
                         }
-                        if (types.length === 1) {
-                            let colorBg = `var(--${types})`
+                        if (typesBg.length === 1) {
+                            let colorBg = `var(--${typesBg})`
                             cardBg.style.background = colorBg
                         } else {
-                            let colorBg = `linear-gradient(90deg, var(--${types[0]}), var(--${types[1]}))`
+                            let colorBg = `linear-gradient(90deg, var(--${typesBg[0]}), var(--${typesBg[1]}))`
                             cardBg.style.backgroundImage = colorBg
                         }
                     }
 
                     function changeStatus() {
                         const statsApi = data.stats
-                        const statsPlace = document.querySelectorAll('.rawStats')
-                        statsPlace.forEach(statsP => {
-                            let statsID = statsP.id
-                            let i = statsID.substring(5)
-                            let statsName = statsApi[i].stat.name
+                        const statsUl = document.querySelector('.stats')
+                        if (statsApi !== statsUl) {
+                            statsUl.innerHTML = ''
+                        }
+                        for (let i = 0; i < statsApi.length; i++) {
+                            let statsName = statsApi[i].stat.name;
                             let statsValue = statsApi[i].base_stat
-                            statsP.innerHTML = `${statsName[0].toUpperCase()}${statsName.substring(1).replace('-', ' ')}: ${statsValue}`
-                        })
+                            statsUl.innerHTML += `<li><p>${statsName[0].toUpperCase()}${statsName.substring(1).replace('-', ' ')}:</p> <i>${statsValue}</i>`
+                        }
                     }
 
                     function changeSkills() {
